38227cb29476c3e0d8700ee110f30ce35b91a916
yin-bp <yin-bp@163.com>
2015-09-19 10:38:40 +0800

    修改测试用例

--- persistent/src/com/frameworkset/sqlexecutor/CustomPoolTest.java
+++ persistent/src/com/frameworkset/sqlexecutor/CustomPoolTest.java
@@ -17,12 +17,14 @@ public class CustomPoolTest {
 	@BeforeClass
 	public static void init()
 	{
-		SQLUtil.startPool("test","oracle.jdbc.driver.OracleDriver",
+		SQLUtil.startPool("test",//数据源名称
+				"oracle.jdbc.driver.OracleDriver",//oracle驱动
 				"jdbc:oracle:thin:@(description=(address_list=(load_balance=off)(failover=on)"
-				+ "(address=(protocol=tcp)(host=10.0.8.226)(port=1521))(address=(protocol=tcp)(host=10.0.8.227)(port=1521)))"
-				+ "(connect_data=(service_name=PF1)(failover_mode=(type=select)(method=basic)(RETRIES=180)(DELAY=5))))",
-				"mdm","Y6^21rtDFuTYEWR#s",
-				 "select 1 from dual");//初始化数据源
+				+ "(address=(protocol=tcp)(host=192.168.1.226)(port=1521))(address=(protocol=tcp)(host=192.168.1.227)(port=1521)))"
+				+ "(connect_data=(service_name=PF1)(failover_mode=(type=select)(method=basic)(RETRIES=180)(DELAY=5))))",//oracle rac链接串
+				"mdm","123456",//数据库账号和口令
+				 "select 1 from dual" //数据库连接校验sql
+				);//初始化数据源
 	}
 	@Test
 	public void initdata() throws SQLException
@@ -37,6 +39,35 @@ public class CustomPoolTest {
 		}
 		
 		SQLExecutor.insertWithDBName("test", "insert into TB_MODULE_AUTH(AUTH_ID,ADMIN_ID,MODULE_ID) values(?,'23',?)", aid+"","11");///SanyUIMPC/page/SanyAccount/orgTree.jsp
+	}
+	
+	public void testsqlite(String dbpath)
+	{
+		//启动sqlite数据源gencode
+		SQLUtil.startPool("gencode","org.sqlite.JDBC","jdbc:sqlite://"+dbpath,"root","root",
+				 "select 1"				
+	        		);
+		//判断数据源gencode中对于的表BBOSS_GENCODE是否存在，不存在则创建
+		String exist = "select 1 from BBOSS_GENCODE";
+		
+		try {
+			SQLExecutor.queryObjectWithDBName(int.class,"gencode", exist);
+		} catch (Exception e) {
+			String tsql = "create table BBOSS_GENCODE (ID string,TABLENAME string,DBNAME string,FIELDINFOS TEXT,AUTHOR string,"
+				       + "COMPANY string,"
+				       + "CREATETIME number(10),"
+				       + "UPDATETIME number(10),"
+				       + "CONTROLPARAMS TEXT,"
+				       + "  PRIMARY KEY (ID))";
+			 
+			try {
+				SQLExecutor.updateWithDBName("gencode",tsql);
+				 
+			} catch (SQLException e1) {
+				 
+				e1.printStackTrace();
+			}
+		}
 	}
 
 }
