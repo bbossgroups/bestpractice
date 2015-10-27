1f3a553229c7cd9959dafc0e0695b4c67a289ed0
yin-bp <yin-bp@163.com>
2015-10-27 15:32:02 +0800

    1.将bboss mvc官网和官网demo剥离成bboss-site一级项目，bboss-mvc工程中只保留mvc核心工程 2.将mvc框架中的标签库全部迁移到bboss-taglib工程中 3.将rpc框架剥离成bboss-rpc一级项目 4.将word plugin和hibernate plugin，word jod项目剥离到bboss-plugin项目中

--- persistent/src/com/frameworkset/sqlexecutor/CustomPoolTest.java
+++ persistent/src/com/frameworkset/sqlexecutor/CustomPoolTest.java
@@ -19,26 +19,25 @@ public class CustomPoolTest {
 	{
 		SQLUtil.startPool("test",//数据源名称
 				"oracle.jdbc.driver.OracleDriver",//oracle驱动
-				"jdbc:oracle:thin:@(description=(address_list=(load_balance=off)(failover=on)"
-				+ "(address=(protocol=tcp)(host=192.168.1.226)(port=1521))(address=(protocol=tcp)(host=192.168.1.227)(port=1521)))"
-				+ "(connect_data=(service_name=PF1)(failover_mode=(type=select)(method=basic)(RETRIES=180)(DELAY=5))))",//oracle rac链接串
-				"mdm","123456",//数据库账号和口令
+				"jdbc:oracle:thin:@(description=(address_list=(load_balance=off)(failover=on)(address=(protocol=tcp)(host=10.0.8.226)(port=1521))(address=(protocol=tcp)(host=10.0.8.227)(port=1521)))(connect_data=(service_name=PF1)(failover_mode=(type=select)(method=basic)(RETRIES=180)(DELAY=5))))",//oracle rac链接串
+				"mdm","Y6^21rtDFuTYEWR#s",//数据库账号和口令
 				 "select 1 from dual" //数据库连接校验sql
 				);//初始化数据源
 	}
 	@Test
 	public void initdata() throws SQLException
 	{
-		List<String> data = SQLExecutor.queryListWithDBName(String.class,"test","select MODULE_ID from TB_MODULE_INFO where MODULE_URL like ?","%/CrmAccount/%");
+//		List<String> data = SQLExecutor.queryListWithDBName(String.class,"test","select MODULE_ID from TB_MODULE_INFO where MODULE_URL like ?","%/CrmAccount/%");
+		List<String> data = SQLExecutor.queryListWithDBName(String.class,"test","select MODULE_ID from TB_MODULE_INFO  ");
 		int aid  =SQLExecutor.queryObjectWithDBName(int.class, "test","select max(to_number(AUTH_ID)) from TB_MODULE_AUTH");
 		aid ++;
 		for(String mid:data)
 		{
-			SQLExecutor.insertWithDBName("test", "insert into TB_MODULE_AUTH(AUTH_ID,ADMIN_ID,MODULE_ID) values(?,'23',?)", aid+"",mid);//'22'是管理账号的id，对应于TD_USER_ADMIN表的SEQNO的值
+			SQLExecutor.insertWithDBName("test", "insert into TB_MODULE_AUTH(AUTH_ID,ADMIN_ID,MODULE_ID) values(?,'25',?)", aid+"",mid);//'22'是管理账号的id，对应于TD_USER_ADMIN表的SEQNO的值
 			aid = aid+1;
 		}
 		
-		SQLExecutor.insertWithDBName("test", "insert into TB_MODULE_AUTH(AUTH_ID,ADMIN_ID,MODULE_ID) values(?,'23',?)", aid+"","11");///SanyUIMPC/page/SanyAccount/orgTree.jsp
+//		SQLExecutor.insertWithDBName("test", "insert into TB_MODULE_AUTH(AUTH_ID,ADMIN_ID,MODULE_ID) values(?,'25',?)", aid+"","11");///SanyUIMPC/page/SanyAccount/orgTree.jsp
 	}
 	
 	public void testsqlite(String dbpath)
