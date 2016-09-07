016ca729780e63bbe34ce11b1bfc72deeca1f188
yin-bp <yin-bp@163.com>
2016-09-07 23:09:13 +0800

    完善mvcdemo http cache示例，增加etag示例方法
    修改mvc demo readme手册

--- mvc/src/org/frameworkset/mvc/HttpCache.java
+++ mvc/src/org/frameworkset/mvc/HttpCache.java
@@ -6,11 +6,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.frameworkset.http.HttpHeaders;
 import org.frameworkset.http.HttpStatus;
 import org.frameworkset.http.ResponseEntity;
+import org.frameworkset.util.DigestUtils;
 import org.frameworkset.util.MultiValueMap;
 import org.frameworkset.util.annotations.RequestHeader;
 import org.frameworkset.util.annotations.RequestParam;
@@ -27,8 +26,7 @@ public class HttpCache {
 		gmtDateFormat.parse("Thu, 1 Jan 1970 08:00:00 GMT");
 	}
 	
-	public ResponseEntity<String> cache(
-		      HttpServletRequest request,
+	public ResponseEntity<String> cache( 
 		      //为了方便测试，此处传入文档最后修改时间
 		      @RequestParam(name="millis") long lastModifiedMillis,
 		      //浏览器验证文档内容是否修改时传入的Last-Modified If-Modified-Since
@@ -58,6 +56,37 @@ public class HttpCache {
 		    //文档生存时间 http 1.1支持
 		    headers.add("Cache-Control", "max-age=" + maxAge);
 		    return new ResponseEntity<String>(body, headers, HttpStatus.OK,"String");
+		}
+	
+	
+	public ResponseEntity<String> etgcache( 
+		      //浏览器验证文档内容的实体 If-None-Match
+		      @RequestHeader (name = "If-None-Match", required = false) String ifNoneMatch) {
+
+		    //当前系统时间
+		    long now = System.currentTimeMillis();
+		    //文档可以在浏览器端/proxy上缓存多久
+		    long maxAge = 10;
+
+		    String body = "<a href=''>点击访问当前链接</a>";
+
+		    //弱实体
+		    String etag = "W/\"" + DigestUtils.md5DigestAsHex(body.getBytes()) + "\"";
+
+		    if(ifNoneMatch != null &&  ifNoneMatch.equals(etag)) {
+		        return new ResponseEntity<String>(HttpStatus.NOT_MODIFIED);
+		    }
+
+		    DateFormat gmtDateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss 'GMT'", Locale.US);
+		    MultiValueMap<String, String> headers = new HttpHeaders();
+
+		    //ETag http 1.1支持
+		    headers.add("ETag", etag); 
+		    //当前系统时间
+		    headers.add("Date", gmtDateFormat.format(new Date(now)));
+		    //文档生存时间 http 1.1支持
+		    headers.add("Cache-Control", "max-age=" + maxAge);
+		    return new ResponseEntity<String>(body, headers, HttpStatus.OK);
 		}
 
 }
