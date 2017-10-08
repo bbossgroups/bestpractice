db135ae499dd9235738913ab009d0ef345036c1f
yinbp <yin-bp@163.com>
2017-10-08 09:32:00 +0800

    更改框架版本号

--- testkafka/src/org/frameworkset/plugin/kafka/StoreServiceTest.java
+++ testkafka/src/org/frameworkset/plugin/kafka/StoreServiceTest.java
@@ -5,24 +5,31 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 
 import kafka.message.MessageAndMetadata;
 
-public class StoreServiceTest implements StoreService {
+import java.util.List;
+
+public class StoreServiceTest extends BaseStoreService {
 	StringDeserializer sd = new StringDeserializer();
 	LongDeserializer ld = new LongDeserializer();
 	@Override
-	public void store(MessageAndMetadata<byte[], byte[]> message) throws Exception {
-		
-		String data = sd.deserialize(null,message.message());
-		long key = 0;
-		if(message.key() != null)
-			key = ld.deserialize(null, message.key());
-		System.out.println("key="+key+",data="+data);
+	public void store(List<MessageAndMetadata<byte[], byte[]>> messages) throws Exception {
+		for(MessageAndMetadata<byte[], byte[]> message:messages){
+			String data = sd.deserialize(null,message.message());
+			long key = ld.deserialize(null, message.key());
+			System.out.println("key="+key+",data="+data);
+		}
 	}
 
 	@Override
 	public void closeService() {
 		sd.close();
 		ld.close();
+	}
 
+	@Override
+	public void store(MessageAndMetadata<byte[], byte[]> message) throws Exception {
+		String data = sd.deserialize(null,message.message());
+		long key = ld.deserialize(null, message.key());
+		System.out.println("key="+key+",data="+data);
 	}
 
 }
